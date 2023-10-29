dcb919f01dbe788305cc3d5841e8afc20e3d3dde
behzadk <root@DESKTOP-NISN4AG.localdomain>
2023-10-29 08:59:13 +0000

    added module for data splitters

---
+++ fpl_ml/data_splitter.py
@@ -0,0 +1,13 @@
+from sklearn.model_selection import train_test_split
+
+
+def randomly_split_data(df, test_frac):
+    if test_frac == 1.0:
+        return df, df[0:0]
+
+    elif test_frac == 0.0:
+        return df[0:0], df
+
+    df_a, df_b = train_test_split(df, test_size=1 - test_frac, shuffle=True)
+
+    return df_a, df_b
