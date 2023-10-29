c1244545836b133dab926957b9d9715d5ba45a3c
behzadk <root@DESKTOP-NISN4AG.localdomain>
2023-10-29 19:12:26 +0000

    refactored user config argument definitiosn

--- user_config.py
+++ user_config.py
@@ -1,6 +1,9 @@
+DATA_DIR = '/mnt/c/Users/behza/Documents/code/data/fpl_ml/'
 
 # Directory where we want to write prepared data
-OUTPUT_DIR = '../data/fpl_ml/'
+PROCESSED_DATA_DIR = f'{DATA_DIR}/processed/'
+
+MLRUNS_DIR = f"{DATA_DIR}/mlruns/"
 
 # Directory where https://github.com/vaastav/Fantasy-Premier-League is cloned to
 VAASTAV_FPL_DIR = '../Fantasy-Premier-League/'
