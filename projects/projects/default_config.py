fc0065a5c9127b60a3ceaf79baccb5d2f46f2b36
behzadk <root@DESKTOP-NISN4AG.localdomain>
2023-11-11 18:41:44 +0000

    moved configs to projects

--- projects/projects/default_config.py
+++ projects/projects/default_config.py
@@ -43,5 +43,5 @@ def collect_config_store():
         ]
     )
 
-    hz.store(config, group="fpl_ml", name="default_config")
+    hz.store(config, group="default", name="default_config")
     hz.store.add_to_hydra_store(overwrite_ok=False)
