fixed precision when fetching from numpy to torch

ml_core/ml_core/dataset.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+from loguru import logger
 
 
 class Dataset(torch.utils.data.Dataset):
@@ -12,6 +13,7 @@ class Dataset(torch.utils.data.Dataset):
         predict: bool = False,
         use_torch: bool = False,
         device: str = "cpu",
+        precision: int = 32
     ):
         self._X = X
         self._y = y
@@ -21,8 +23,10 @@ class Dataset(torch.utils.data.Dataset):
 
         if use_torch:
             # Move all data to device
-            self._X = torch.from_numpy(self._X).float().to(device)
-            self._y = torch.from_numpy(self._y).float().to(device)
+            self._X = torch.from_numpy(self._X.astype(f'float{precision}')).to(device)
+            self._y = torch.from_numpy(self._y.astype(f'float{precision}')).to(device)
+
+            logger.info(self._X.dtype)
 
         self.predict = predict
 