97e4223ffb81a2b4877ad8c6ee262380e33491df
behzadk <root@DESKTOP-NISN4AG.localdomain>
2023-12-10 18:54:28 +0000

    user facing run train module updated

--- run_train.py
+++ run_train.py
@@ -1,18 +1,28 @@
 import os
-from user_config import PROCESSED_DATA_DIR, MLRUNS_DIR
+from user_config import PROCESSED_DATA_DIR, MLRUNS_DIR, DEMO_DATA_DIR
 from fpl_ml_projects import fpl_ml
 
 os.environ["HYDRA_FULL_ERROR"] = "1"
 
 
+def train_demo():
+    # User defined path to our prepared data
+    train_val_data_path = os.path.join(DEMO_DATA_DIR, "diabetes_train.csv")
+    test_data_path = os.path.join(DEMO_DATA_DIR, "diabetes_test.csv")
 
+
+    fpl_ml.train.train_demo(user_mlruns_dir, train_val_data_path, test_data_path,)
+
+def train_fpl_ml():
+    # User defined path to our prepared data
+    train_val_data_path = os.path.join(PROCESSED_DATA_DIR, "train_val_data.csv")
+    test_data_path = os.path.join(PROCESSED_DATA_DIR, "test_data.csv")
+
+    fpl_ml.project_train.train(user_mlruns_dir, train_val_data_path, test_data_path)
 
 if __name__ == "__main__":
     # User defined directory to write our mlruns data
     user_mlruns_dir = MLRUNS_DIR
 
-    # User defined path to our prepared data
-    train_val_data_path = os.path.join(PROCESSED_DATA_DIR, "train_val_data.csv")
-    test_data_path = os.path.join(PROCESSED_DATA_DIR, "test_data.csv")
-
-    fpl_ml.train.train(user_mlruns_dir, train_val_data_path, test_data_path, )
+    # train_demo()
+    train_fpl_ml()
